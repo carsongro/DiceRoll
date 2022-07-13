22ccc5fe6f5341fcb6aae816999836f3b15a6f43
Carson <carsongro@gmail.com>
2022-07-13 13:10:54 -0600

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Carson Gross on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
